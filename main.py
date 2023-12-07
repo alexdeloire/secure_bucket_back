502ebc70a8092e56058c8b64f5eef12cd51e0bdb
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2023-12-07 22:42:46 +0100

    cors

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://climat-secure.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
